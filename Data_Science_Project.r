adec3dcee72ea360bb8505cfca94f73e4b8a70f4
sabneet95 <45975033+sabneet95@users.noreply.github.com>
2020-03-08 12:32:50 -0400

    Create Data_Science_Project.r

---
+++ Data_Science_Project.r
@@ -0,0 +1,39 @@
+# Programmer: Sabneet Bains
+# Purpose: Data Science Project for STAT 201
+# Date: 06/10/18
+
+##############################################################################################
+# 1. Importing original data set of Stack Overflow 2018 Developer Survey
+
+original_data <- read.csv("survey_results_public_2.csv")
+
+# 1.1. Randomly choosing 1000 rows from the original data (*default seed)
+
+random_sample <- original_data[sample(1:nrow(original_data), 1000),]
+
+##############################################################################################
+# 2. Performing basic descriptive statistics on the random sample
+
+sink('random_sample_summary.txt')
+summary(random_sample)
+sink()
+
+##############################################################################################
+# 3. Data visualization of all the yearly salaries in the random sample
+
+hist(random_sample$Yearly.Salary.in.USD, main = "Salary Distribution Amongst Stock Overflow Developer Survey Respondents", ylab = "Number of Respondents", xlab = "Yearly Salary ($)")
+
+##############################################################################################
+# 4. Analysis of the shared variability between "Open Source" and "Yearly Salary in USD" 
+
+developer_opinion <- plot(random_sample$Open.Source, random_sample$Yearly.Salary.in.USD, main = "Stock Overflow Developer Survey: Respondent Yearly Salary ($) vs Stance on Open Source Software", ylab = "Yearly Salary ($)", xlab = "Stance on open source software", col=7:6)
+
+
+developer_opinion_no <- developer_opinion$stats[,1]
+developer_opinion_yes <- developer_opinion$stats[,2]
+
+sink('final_analysis.txt')
+print('    Min. Lower Qu. Median Upper Qu. Max')
+print(developer_opinion_no)
+print(developer_opinion_yes)
+sink()
